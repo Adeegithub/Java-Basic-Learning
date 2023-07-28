a290a29f3dd67e8c080c5ade7c97d76d0bde308a
Adeegithub <arunodagunawardana@gmail.com>
2023-07-28 13:05:31 +0530

    "Second Commit"

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+//        //USING SCANNER
+//
+//        //Creating an object to the 45scanner
+//        Scanner x = new Scanner(System.in);
+//
+//        //Prompt for the input
+//        System.out.print("Enter your name: ");
+//
+//        //Using the above created object(x) to store the value of the user input
+//        String name = x.nextLine(); //String Value nextLine()
+//
+//        //Outputting the name
+//        System.out.println("Hello " + name);
+//
+//        System.out.print("Enter your age: ");
+//        //Reading an Integer value nextInt()
+//        int age = x.nextInt();
+//        System.out.println("Your age is: " + age);
+//
+//
+//        //USING MATHEMATICAL OPERATORS
+//
+//        int num1 = 10;
+//        int num2 = 5;
+//        int total = num1 + num2;
+//        int difference = num1 - num2;
+//        int multiply = num1 * num2;
+//        int division = num1 / num2;
+//        int mod = num1 % num2;
+//
+//        System.out.println("Number 1 is: " + num1 + " Number 2 is: " + num2);
+//        System.out.println("The Sum is: " + total);
+//        System.out.println("The difference is: " + difference);
+//        System.out.println("The multiplication is: " + multiply);
+//        System.out.println("The division is: " + division);
+//        System.out.println("The mod is: " + mod);
+
+        //ASSIGNMENT OPERATORS
+
+//        //Post Increment
+//        int p = 10;
+//        // 1 -> First substitute the value of p to q
+//        // 2 -> Then the Increment
+//        int q = p++;
+//        System.out.println("The value of p is: " + p);
+//        System.out.println("The value of q is: " + q);
+//
+//        //Pre Increment
+//        int p1 = 15;
+//        // 1 -> First the increment
+//        // 2 -> Then Substitute the value of p1 to q1
+//        int q1 = ++p1;
+//        System.out.println("The value of p1 is: " + p1);
+//        System.out.println("The value of q1 is: " + q1);
+
+        //IF-ELSE AND COMPARISON OPERATORS
+//        Scanner sc1 = new Scanner(System.in);
+//        System.out.print("Enter your average: ");
+//        double average = sc1.nextInt();
+//
+//        if (average >= 75) {
+//            System.out.println("You got A pass");
+//        } else if (average >= 65) {
+//            System.out.println("You got B pass");
+//        } else if (average >= 55) {
+//            System.out.println("You got C pass");
+//        } else if (average >= 45) {
+//            System.out.println("You got S pass");
+//        } else {
+//            System.out.println("Sorry, You failed");
+//        }
+//
+//        if (average >= 45 && average < 100) {
+//            System.out.println("You are Good");
+//        } else {
+//            System.out.println("Please work Harder");
+//        }
+
+
+    }
+}
